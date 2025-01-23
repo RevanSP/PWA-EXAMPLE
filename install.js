07757d7e9ad88ac96d5a585d40afd6c7067ecf41
RevanSP <revanspstudy28@gmail.com>
2025-01-23 12:45:38 +0700

    first commit

---
+++ install.js
@@ -0,0 +1,22 @@
+let deferredPrompt; 
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+
+  const installBtn = document.getElementById('install-btn');
+  installBtn.style.display = 'block';
+
+  installBtn.addEventListener('click', () => {
+    deferredPrompt.prompt();
+
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+  });
+});
